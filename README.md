33d5d65b1b1fa16932267492bfd2ed9673062c71
Austin Harrison <osagiedeharrison@yahoo.com>
2021-09-09 19:34:03 +0100

    initial production release

---
+++ README.md
@@ -0,0 +1,1 @@
+# Express session Authentication
---
+++ server.js
@@ -0,0 +1,28 @@
+// Libraries are arranged alphabetically
+
+const dotenv = require("dotenv");
+const express = require("express");
+
+// Config setup
+dotenv.config({ path: "config.env" });
+
+const app = express();
+// Initial Production Mode
+app.use((req, res, next) => {
+  res.status(500).json({
+    message: "Sorry for the delay, virtual app will be running soon",
+  });
+  next();
+});
+
+// For errors
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    message: "Sorry, the problem is from us, please co-operate with us!",
+  });
+  next();
+});
+
+app.listen(5000, () =>
+  console.log(`server is running at PORT ${process.env.PORT}`)
+);
